06b5ac6e97c74779919afbe4e82ea1b64adc9a93
azadeh.samadi <samadi.azadeh@gmail.com>
2023-08-01 10:34:53 +0200

    exercise2

---
+++ src/main/java/com/exercise2/App.java
@@ -0,0 +1,19 @@
+package com.exercise2;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+
+        int [] i =new int[]{11,23,39 };
+        System.out.print(i[0]+" ");
+        System.out.print(i[1]+" ");
+        System.out.println(i[2]+" ");
+        System.out.println(".......");
+
+
+    }
+
+
+}
